f5c1eccd5be60de07bf7fb37d1cd8fb9e4b41410
0xMasayoshi <0xMasayoshi@protonmail.com>
2024-01-29 23:16:37 +0800

    chore: PoolPageV2

--- apps/evm/src/app/pool/[id]/(landing)/page.tsx
+++ apps/evm/src/app/pool/[id]/(landing)/page.tsx
@@ -1,16 +1,9 @@
 import { PoolProtocol } from '@sushiswap/rockset-client'
-import { Container, Separator } from '@sushiswap/ui'
 import { unstable_cache } from 'next/cache'
 import { getPool } from 'src/lib/flair/fetchers/pool/id/pool'
+import { PoolPageV2 } from 'src/ui/pool/PoolPageV2'
 import { ID, unsanitize } from 'sushi'
-import { UnknownTokenAlert } from '../../../../ui/pool'
-import { ManageV2LiquidityCard } from '../../../../ui/pool/ManageV2LiquidityCard'
-import { PoolChartV2 } from '../../../../ui/pool/PoolChartV2'
-import { PoolComposition } from '../../../../ui/pool/PoolComposition'
 import { PoolPageV3 } from '../../../../ui/pool/PoolPageV3'
-import { PoolRewards } from '../../../../ui/pool/PoolRewards'
-import { PoolStats } from '../../../../ui/pool/PoolStats'
-import { PoolTransactionsV2 } from '../../../../ui/pool/PoolTransactionsV2'
 import notFound from '../../not-found'
 
 export default async function PoolPage({
@@ -37,43 +30,5 @@ export default async function PoolPage({
     return <PoolPageV3 pool={pool} />
   }
 
-  return (
-    <Container maxWidth="5xl" className="px-2 sm:px-4">
-      <UnknownTokenAlert pool={pool} />
-      <div className="flex flex-col gap-6">
-        <div className="grid grid-cols-1 md:grid-cols-[auto_400px] gap-6">
-          <div>
-            <ManageV2LiquidityCard pool={pool} tab={tab} />
-          </div>
-          <div className="flex flex-col gap-6">
-            {/* <PoolPositionProvider pool={pool}>
-              <PoolPositionStakedProvider pool={pool}>
-                <PoolPositionRewardsProvider pool={pool}>
-                  <PoolPosition pool={pool} />
-                  <PoolMyRewards pool={pool} />
-                </PoolPositionRewardsProvider>
-              </PoolPositionStakedProvider>
-            </PoolPositionProvider> */}
-          </div>
-        </div>
-        <div className="py-4">
-          <Separator />
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-[auto_400px] gap-6">
-          <div>
-            <PoolChartV2 id={poolId as ID} />
-          </div>
-          <div className="flex flex-col gap-6">
-            <PoolComposition pool={pool} />
-            <PoolStats pool={pool} />
-            <PoolRewards pool={pool} />
-          </div>
-        </div>
-        <div className="py-4">
-          <Separator />
-        </div>
-        <PoolTransactionsV2 pool={pool} />
-      </div>
-    </Container>
-  )
+  return <PoolPageV2 pool={pool} tab={tab} />
 }
