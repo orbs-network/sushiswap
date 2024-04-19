f4a23513795c81d41be3269bd48ff6553030c86c
Matthew Lilley <hello@matthewlilley.com>
2024-04-19 09:33:43 +0100

    chore: explore should be a single page link

--- apps/tron/src/app/header.tsx
+++ apps/tron/src/app/header.tsx
@@ -1,48 +1,14 @@
 'use client'
 
-import { classNames } from '@sushiswap/ui'
 import { SushiIcon } from '@sushiswap/ui/components/icons'
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from '@sushiswap/ui/components/navigation-menu'
-import Link from 'next/link'
 import React, { FC } from 'react'
-
-interface NavigationListItemProps extends React.ComponentPropsWithoutRef<'a'> {
-  href: string
-}
-
-const NavigationListItem: FC<NavigationListItemProps> = ({
-  className,
-  title,
-  children,
-  href,
-}) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <Link
-          href={href}
-          className={classNames(
-            'cursor-pointer block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-            className,
-          )}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="text-sm leading-snug line-clamp-2 text-muted-foreground">
-            {children}
-          </p>
-        </Link>
-      </NavigationMenuLink>
-    </li>
-  )
-}
 
 export const Navigation: FC = () => {
   return (
@@ -69,32 +35,12 @@ export const Navigation: FC = () => {
             </NavigationMenuItem>
 
             <NavigationMenuItem className="hidden lg:block">
-              <NavigationMenuTrigger>Explore</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="min-w-[240px] gap-3 p-4">
-                  <NavigationListItem
-                    key="pools"
-                    title="Pools"
-                    href="/explore/pools"
-                  >
-                    Pools
-                  </NavigationListItem>
-                  <NavigationListItem
-                    key="tokens"
-                    title="Tokens"
-                    href="/explore/tokens"
-                  >
-                    Tokens
-                  </NavigationListItem>
-                  <NavigationListItem
-                    key="transactions"
-                    title="Transactions"
-                    href="/explore/transactions"
-                  >
-                    Transactions
-                  </NavigationListItem>
-                </ul>
-              </NavigationMenuContent>
+              <NavigationMenuLink
+                href="/explore"
+                className={navigationMenuTriggerStyle()}
+              >
+                Explore
+              </NavigationMenuLink>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
